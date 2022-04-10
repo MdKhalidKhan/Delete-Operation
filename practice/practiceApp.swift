0d6e046d9215e770994cfb8b9c4a2a277975f38f
Raj <rajkhulna@outlook.com>
2022-04-11 01:29:50 +0600

    Initial Commit

---
+++ practice/practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  practiceApp.swift
+//  practice
+//
+//  Created by Khalid Khan on 11/4/22.
+//
+
+import SwiftUI
+
+@main
+struct practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
